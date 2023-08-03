c39651bb9505fe3168ff1d3e7d39ccf1bb7458f7
Chinzicam <a3189797316@163.com>
2023-08-03 16:26:17 +0800

    :fire: 缓存穿透

--- hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -16,8 +16,7 @@ import javax.annotation.Resource;
 
 import java.util.concurrent.TimeUnit;
 
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
-import static com.hmdp.utils.RedisConstants.CACHE_SHOP_TTL;
+import static com.hmdp.utils.RedisConstants.*;
 
 /**
  * <p>
@@ -41,16 +40,29 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
      */
     @Override
     public Result queryById(Long id) {
+        //从redis查询数据
         String key = CACHE_SHOP_KEY + id;
         String shopJson = stringRedisTemplate.opsForValue().get(key);
+        // 判断缓存是否存在
         if (StrUtil.isNotBlank(shopJson)) {
+            // 存在，直接返回
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
         }
+        // 判断命中的是否是空值
+        if("".equals(shopJson)){
+            // 返回一个错误信息
+            return Result.fail("商铺信息不存在");
+        }
+
+        // 缓存不存在，根据id查询数据库
         Shop shop = getById(id);
+        // 数据库不存在，返回错误
         if(shop == null) {
+            stringRedisTemplate.opsForValue().set(key,"",CACHE_NULL_TTL, TimeUnit.MINUTES);
             return Result.fail("商铺不存在");
         }
+        // 数据库存在，写入redis
         stringRedisTemplate.opsForValue().set(key,JSONUtil.toJsonStr(shop),CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
     }
