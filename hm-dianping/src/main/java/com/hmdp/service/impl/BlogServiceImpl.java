3b5d80d17e22a10c3ae8024622e4a826f35b7b35
Chinzicam <a3189797316@163.com>
2023-08-14 21:21:21 +0800

    :sparkles: 点赞

--- hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,7 +1,9 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.BooleanUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
@@ -9,10 +11,14 @@ import com.hmdp.service.IBlogService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.BLOG_LIKED_KEY;
 
 /**
  * <p>
@@ -29,9 +35,12 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     private IBlogService blogService;
     @Resource
     private IUserService userService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 查询首页热门笔记，返回封面信息
+     *
      * @param current
      * @return
      */
@@ -45,16 +54,15 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         List<Blog> records = page.getRecords();
         // 查询用户
         records.forEach(blog -> {
-            Long userId = blog.getUserId();
-            User user = userService.getById(userId);
-            blog.setName(user.getNickName());
-            blog.setIcon(user.getIcon());
+            queryBlogById(blog);
+            isBlogLiked(blog);
         });
         return Result.ok(records);
     }
 
     /**
      * 根据id查询笔记信息，返回笔记信息和个人信息
+     *
      * @param id
      * @return
      */
@@ -64,10 +72,66 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         if (blog == null) {
             return Result.fail("笔记不存在");
         }
+        queryBlogById(blog);
+        isBlogLiked(blog);
+        return Result.ok(blog);
+    }
+
+
+    /**
+     * 根据id查询昵称头像
+     * @param blog
+     */
+    private void queryBlogById(Blog blog) {
         Long userId = blog.getUserId();
         User user = userService.getById(userId);
         blog.setName(user.getNickName());
         blog.setIcon(user.getIcon());
-        return Result.ok(blog);
+    }
+
+    /**
+     * 笔记点赞功能
+     * @param id
+     * @return
+     */
+    @Override
+    public Result likeBlog(Long id) {
+        //获取当前用户信息
+        Long userId = UserHolder.getUser().getId();
+        //如果当前用户未点赞，则点赞数 +1，同时将用户加入set集合
+        String key = BLOG_LIKED_KEY + id;
+        Double score = stringRedisTemplate.opsForZSet().score(key, userId.toString());
+        //为null，则表示集合中没有该用户,点赞数加一
+        if (score == null) {
+            boolean isSuccess = update().setSql("liked=liked+1").eq("id", id).update();
+            if (isSuccess) {
+                stringRedisTemplate.opsForZSet().add(key, userId.toString(), System.currentTimeMillis());
+            }
+        }
+        //存在用户，则点赞数减一
+        else {
+            boolean success = update().setSql("liked=liked-1").eq("id", id).update();
+            if (success) {
+                stringRedisTemplate.opsForZSet().remove(key, userId.toString());
+            }
+        }
+        return Result.ok();
+    }
+
+    /**
+     * 判断当前笔记是否点赞
+     * @param blog
+     */
+    private void isBlogLiked(Blog blog) {
+
+        UserDTO userDTO = UserHolder.getUser();
+        //当用户未登录时，就不判断了，直接return结束逻辑
+        if (userDTO == null) {
+            return;
+        }
+        //2. 判断当前用户是否点赞
+        String key = BLOG_LIKED_KEY + blog.getId();
+        Double score = stringRedisTemplate.opsForZSet().score(key, userDTO.getId().toString());
+        blog.setIsLike(score != null);
     }
 }
