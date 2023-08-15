d82dda1b3f871f8b8584aa4d99f23e3c7055a787
Chinzicam <a3189797316@163.com>
2023-08-15 10:23:49 +0800

    :sparkles: 关注与取关

--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,14 +1,19 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +22,47 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+
+    @Resource
+    private IFollowService followService;
+
+    /**
+     * 关注与取关
+     *
+     * @param followUserId
+     * @param isFollow
+     * @return
+     */
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        Long userId = UserHolder.getUser().getId();
+        //判断是否关注
+        if (isFollow) {
+            //关注，则将信息保存到数据库
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            followService.save(follow);
+        } else {
+            //取关，则将数据从数据库中移除
+            LambdaQueryWrapper<Follow> wrapper = new LambdaQueryWrapper<>();
+            wrapper.eq(Follow::getUserId, userId).eq(Follow::getFollowUserId, followUserId);
+            followService.remove(wrapper);
+        }
+        return Result.ok();
+    }
+
+    /**
+     * 查询是否关注
+     *
+     * @param followUserId
+     * @return
+     */
+    @Override
+    public Result isFollow(Long followUserId) {
+        LambdaQueryWrapper<Follow> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Follow::getFollowUserId, followUserId);
+        int count = followService.count(wrapper);
+        return Result.ok(count > 0);
+    }
 }
